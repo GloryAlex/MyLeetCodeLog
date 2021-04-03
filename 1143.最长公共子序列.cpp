7cb1ca4fd5c394522c0a6187a352cca579e4ce0b
GloryAlex <GloryXie98@outlook.con>
2021-04-03 08:11:25 +0800

    1143

---
+++ 1143.最长公共子序列.cpp
@@ -0,0 +1,35 @@
+#include "LeetCode.h"
+using namespace std;
+
+class Solution
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp = vector<int>(vector<int>(n + 1), m + 1);
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (text1[i] == text2[j])
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                else
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1], j);
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+int main()
+{
+    ifstream in("input");
+    while (!in.eof())
+    {
+        string s1, s2;
+        in >> s1 >> s2;
+        s1 = getString(s1), s2 = getString(s2);
+        print(Solution().longestCommonSubsequence(s1, s2));
+    }
+}
