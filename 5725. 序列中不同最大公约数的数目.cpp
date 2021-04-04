5f883493ae9ab91272f8834540997d387b8c83c2
GloryAlex <GloryXie98@outlook.con>
2021-04-04 16:10:06 +0800

    235-下午

--- 5725. 序列中不同最大公约数的数目.cpp
+++ 5725. 序列中不同最大公约数的数目.cpp
@@ -3,15 +3,41 @@ using namespace std;
 
 class Solution
 {
+    const int MOD=1e9+7;
+    const int MAX_SIZE=2e5+1;
+    vector<int> isInNums=vector<int>(MAX_SIZE);
+    int maxOfNums;
     inline int gcd(int a,int b) {
-        if(!a||!b)return 0;
         while(b^=a^=b^=a%=b);
         return a;
     }
-    public:
+    bool isOneOfGCDs(int x){
+        int curGcd = -1;
+        for(int i=x;i<=maxOfNums;i+=x){
+            if(isInNums[i]){
+                if(curGcd==-1)curGcd=i;
+                else curGcd=gcd(curGcd,i);
+
+                if(curGcd==x)return true;
+            }
+        }
+        return curGcd==x;
+    }
+public:
     int countDifferentSubsequenceGCDs(vector<int> &nums)
     {
-        //TODO
+        //init neccessary values
+        maxOfNums = -1;
+        for(int i:nums){
+            isInNums[i]=true;
+            maxOfNums=max(maxOfNums,i);
+        }
+        //check all elements in range
+        long long total=0;
+        for(int i=1;i<=maxOfNums;i++){
+            total+=isOneOfGCDs(i);
+        }
+        return total%MOD;
     }
 };
 
