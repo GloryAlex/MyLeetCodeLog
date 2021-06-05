19b90d186ff140c35359c28cb46e24eef8a4241a
GloryAlex <55772529+GloryAlex@users.noreply.github.com>
2021-06-05 14:47:59 +0800

    2021-6-5 test

--- 376.摆动序列.cpp
+++ 376.摆动序列.cpp
@@ -1,32 +1,21 @@
 #include "LeetCode.h"
 using namespace std;
 
-class Solution
-{
-public:
-    int wiggleMaxLength(vector<int> &nums)
-    {
+class Solution {
+   public:
+    int wiggleMaxLength(vector<int> &nums) {
         int n = nums.size();
-        if (n <= 1)
-            return n;
+        if (n <= 1) return n;
 
         int up = 1, down = 1;
-        for (int i = 1; i < n; i++)
-        {
-            if (nums[i] > nums[i - 1])
-            {
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
                 //呈上升趋势
-                if (down < up + 1)
-                    down = up + 1;
-            }
-            else if (nums[i] < nums[i - 1])
-            {
+                if (down < up + 1) down = up + 1;
+            } else if (nums[i] < nums[i - 1]) {
                 //呈下降趋势
-                if (up < down + 1)
-                    up = down + 1;
-            }
-            else
-            {
+                if (up < down + 1) up = down + 1;
+            } else {
                 //什么也不做
             }
         }
@@ -34,11 +23,9 @@ public:
     }
 };
 
-int main()
-{
+int main() {
     ifstream in("input");
-    while (!in.eof())
-    {
+    while (!in.eof()) {
         string str;
         in >> str;
         auto num = getArray(str);
