c4f68b04a2c9e9d7dfdc0a3e6503d76fb72aaaa1
GloryAlex <55772529+GloryAlex@users.noreply.github.com>
2021-06-22 08:50:53 +0800

    需要继续debug 401.二进制手表

--- 401.二进制手表.cpp
+++ 401.二进制手表.cpp
@@ -8,28 +8,36 @@ using namespace std;
 
 // @lc code=start
 class Solution {
-  vector<string> ret;
+    vector<string> ret;
 
- public:
-  vector<string> readBinaryWatch(int turnedOn) {
-    if (turnedOn > 8) return {};  //不可能同时亮超过8个灯
-  }
-  void dfs(int mask, int index, int remain) {
-    if (index >= 10 || remain > 10 - index) return;
-    if(remain==0)ret.push_back(toTime(mask));
-    dfs(mask, index + 1, remain);
-    dfs(mask | (1 << index), index + 1, remain - 1);
-  }
-  string toTime(int mask){
-    //todo
-  }
+   public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        if (turnedOn > 8) return {};  //不可能同时亮超过8个灯
+        dfs(0,0,turnedOn);
+        return ret;
+    }
+    void dfs(int mask, int index, int remain) {
+        if (index >= 10 || remain > 10 - index) return;
+        if (remain == 0){
+          ret.push_back(toTime(mask));
+          return;
+        }
+        dfs(mask, index + 1, remain);
+        dfs(mask | (1 << index), index + 1, remain - 1);
+    }
+    string toTime(int mask) {
+        int hour = (mask & ((1 << 4) - 1));
+        int second = mask >> 4;
+        
+        return to_string(hour)+(second<10?":0":":")+to_string(second);
+    }
 };
 // @lc code=end
 int main() {
-  ifstream in("input");
-  while (!in.eof()) {
-    int lightCount;
-    in >> lightCount;
-    print(Solution().readBinaryWatch(lightCount));
-  }
+    ifstream in("input");
+    while (!in.eof()) {
+        int lightCount;
+        in >> lightCount;
+        print(Solution().readBinaryWatch(lightCount));
+    }
 }
