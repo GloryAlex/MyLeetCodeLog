bf019f783b60dba019a29ed49b3f116f9de0b377
GloryAlex <gloryxie98@outlook.com>
2021-08-08 15:08:13 +0800

    🎨 421

--- 421.数组中两个数的最大异或值.cpp
+++ 421.数组中两个数的最大异或值.cpp
@@ -9,33 +9,56 @@ using namespace std;
 // @lc code=start
 class Solution {
    public:
-   struct TrieNode{
-       bool isEnd;//只有1和0
-       TrieNode* oneNext = nullptr;
-       TrieNode* zeroNext = nullptr;
-   };
-   void insert(TrieNode* root,int number){
-       auto binary = bitset<32>(number);
-       for(int i=31;i>=0;i--){
-           int cur = binary[i];
-           if(cur){
-               if(root->oneNext==nullptr)root->oneNext=new TrieNode();
-                root=root->oneNext;
-           }else{
-               if(root->zeroNext=nullptr)root->zeroNext=new TrieNode();
-               root=root->zeroNext;
-           }
-       }
-   }
-   int getMaxXor(TrieNode* root,int number){
-       auto binary = bitset<32>(number);
-       //TODO: 完成异或函数的编写
-   }
+    struct TrieNode {
+        TrieNode* one = nullptr;
+        TrieNode* zero = nullptr;
+        ~TrieNode(){
+            if(one)delete one;
+            if(zero)delete zero;
+        }
+    };
+    void insert(TrieNode* root, int number) {
+        auto binary = bitset<32>(number);
+        for (int i = 31; i >= 0; i--) {
+            int cur = binary[i];
+            if (cur) {
+                if (root->one == nullptr) root->one = new TrieNode();
+                root = root->one;
+            } else {
+                if (root->zero == nullptr) root->zero = new TrieNode();
+                root = root->zero;
+            }
+        }
+    }
+    int getMaxXor(TrieNode* root, int number) {
+        bitset<32> binary(number);
+        int ret = 0;
+        for (int i = 31; i >= 0; i--) {
+            ret <<= 1;
+            if (binary[i]) {
+                if (root->zero) {
+                    ret |= 0x1;
+                    root = root->zero;
+                } else {
+                    root = root->one;
+                }
+            } else {
+                if (root->one) {
+                    ret |= 0x1;
+                    root = root->one;
+                } else {
+                    root = root->zero;
+                }
+            }
+        }
+        return ret;
+    }
     int findMaximumXOR(vector<int>& nums) {
         TrieNode* root = new TrieNode();
-        for(int i:nums)insert(root,i);
-        int ret=0;
-        for(int i:nums)ret=max(ret,getMaxXor(root,i));
+        for (int i : nums) insert(root, i);
+        int ret = 0;
+        for (int i : nums) ret = max(ret, getMaxXor(root, i));
+        delete root;
         return ret;
     }
 };
