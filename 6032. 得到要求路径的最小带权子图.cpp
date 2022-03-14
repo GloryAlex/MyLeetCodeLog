7d6477f61a64e8fb88d9e6a0ea7202dd749de2f2
gloryxie <gloryxie98@outlook.com>
2022-03-14 15:49:53 +0800

    ✅ 6032

--- 6032. 得到要求路径的最小带权子图.cpp
+++ 6032. 得到要求路径的最小带权子图.cpp
@@ -3,29 +3,78 @@ using namespace std;
 
 class Solution {
    public:
-    vector<vector<int>> graph;
-    const int INF = 1e9;
+    const long long INF = LONG_MAX;
+    using Graph = vector<unordered_map<int, long long>>;
+    struct Point {
+        int next;
+        long long weight;
+        bool operator<(const Point b) const { return weight > b.weight; }
+        static Point valueOf(const pair<int, int> pii) {
+            return {pii.first, pii.second};
+        }
+    };
+    vector<long long> dijistra(int src, const Graph& graph, const int n) {
+        priority_queue<Point> q;
+        vector<bool> hasVisit(n, false);
+        vector<long long> distance(n, INF);
+
+        distance[src] = 0;
+        q.push({src, 0});
+        while (!q.empty()) {
+            auto [id, weight] = q.top();
+            q.pop();
+            if (hasVisit[id])
+                continue;
+            else
+                hasVisit[id] = true;
+
+            for (const auto [j, weight] : graph[id]) {
+                if (distance[j] > distance[id] + weight) {
+                    distance[j] = distance[id] + weight;
+                    q.push({j, distance[j]});
+                }
+            }
+        }
+        return distance;
+    }
     long long minimumWeight(int n, vector<vector<int>>& edges, int src1,
                             int src2, int dest) {
-        graph = vector<vector<int>>(n, vector<int>(n, INF));
+        auto graph = Graph(n);
+        auto reGraph = Graph(n);
         for (const auto& edge : edges) {
-            int begin = edge[0], end = edge[1], weight = edge[2];
-            graph[begin][end] = min(graph[begin][end], weight);
-        }
-        // todo
-    }
+            int begin = edge[0], end = edge[1];
+            long long weight = edge[2];
 
+            if (graph[begin].count(end)) {
+                graph[begin][end] = min(graph[begin][end], weight);
+            } else {
+                graph[begin][end] = weight;
+            }
+            reGraph[end][begin] = graph[begin][end];
+        }
+
+        auto s1 = dijistra(src1, graph, n);
+        auto s2 = dijistra(src2, graph, n);
+        auto d = dijistra(dest, reGraph, n);
+
+        long long res = INF;
+        for (int i = 0; i < n; i++) {
+            if (s1[i] != INF && s2[i] != INF && d[i] != INF)
+                res = min(res, s1[i] + s2[i] + d[i]);
+        }
+        return res == INF ? -1 : res;
+    }
 };
 
 int main() {
     ifstream in("input");
     while (!in.eof()) {
-        json j;
+        json jj;
         int n;
-        in >> n >> j >> ws;
-        auto arr = j.get<vector<vector<int>>>();
+        in >> n >> jj >> ws;
+        auto arr = jj.get<vector<vector<int>>>();
         int i, j, k;
         in >> i >> j >> k >> ws;
-        print(Solution().minimumWeight(n, arr, i, k, k));
+        print(Solution().minimumWeight(n, arr, i, j, k));
     }
 }
