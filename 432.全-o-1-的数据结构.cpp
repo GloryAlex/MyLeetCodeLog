c7f32f0c9b14d8f834742c5160c9b9e784a6c480
gloryxie <gloryxie98@outlook.com>
2022-03-16 14:18:50 +0800

    🤡 432

--- 432.全-o-1-的数据结构.cpp
+++ 432.全-o-1-的数据结构.cpp
@@ -1,15 +1,6 @@
-// 请你实现一个数据结构支持以下操作：
+#include <list>
 
-// - Inc(key) - 插入一个新的值为 1 的 key。或者使一个存在的 key 增加一，保证 key
-// 不为空字符串。
-// - Dec(key) - 如果这个 key 的值是
-// 1，那么把他从数据结构中移除掉。否则使一个存在的 key 值减一。如果这个 key
-// 不存在，这个函数不做任何事情。key 保证不为空字符串。
-// - GetMaxKey() - 返回 key
-// 中值最大的任意一个。如果没有元素存在，返回一个空字符串"" 。
-// - GetMinKey() - 返回 key
-// 中值最小的任意一个。如果没有元素存在，返回一个空字符串""。
-#include "LeetCode.h"
+#include "lib/leetcode.h"
 using namespace std;
 
 /*
@@ -20,85 +11,62 @@ using namespace std;
 
 // @lc code=start
 class AllOne {
-  struct Node {
-    string key;
-    int val = 0;
-    Node* prev = nullptr;
-    Node* next = nullptr;
-  };
-  Node _dummy_head, _dummy_tail;
-  //必要的基本成员
-  unordered_map<string, Node*> dict;
-  Node* head = &_dummy_head;  //最小值结点的前一个
-  Node* tail = &_dummy_tail;  //最大值结点的后一个
+    using CountList = list<pair<unordered_set<string>, int>>;
+    CountList lst;
+    unordered_map<string, CountList::iterator> dict;
 
- public:
-  /** Initialize your data structure here. */
-  AllOne() {
-    head->next = tail;
-    tail->prev = head;
-  }
-  bool isEmpty() { return head->next == tail && tail->prev == head; }
-  void moveFront(Node* node) {
-    //值变小
-    if (node->prev == head) return;
-    node->prev->next = node->next;
-    node->next->prev = node->prev;
+   public:
+    AllOne() {}
 
-    node->next = node->prev;
-    node->prev = node->prev->prev;
-
-    node->prev->next = node;
-    node->next->prev = node;
-  }
-  void moveBack(Node* node) {
-    //值变大
-    if (node->next == tail) return;
-    node->prev->next = node->next;
-    node->next->prev = node->prev;
-
-    node->prev = node->next;
-    node->next = node->next->next;
-
-    node->prev->next = node;
-    node->next->prev = node;
-  }
-  /** Inserts a new key <Key> with value 1. Or increments an existing key by 1.
-   */
-  void inc(string key) {
-    if (!dict.count(key)) {
-      dict[key] = new Node{key, 0, head, head->next};
-      head->next->prev = dict[key];
-      head->next = dict[key];
+    void inc(string key) {
+        if (dict.count(key)) {
+            auto node = dict[key];
+            auto nextNode = next(node);
+            if (nextNode == lst.end() || nextNode->second != node->second + 1) {
+                dict[key] = lst.emplace(nextNode, unordered_set<string>({key}),
+                                        node->second + 1);
+            } else {
+                dict[key] = nextNode;
+                nextNode->first.insert(key);
+            }
+            node->first.erase(key);
+            if (node->first.empty()) lst.erase(node);
+        } else {
+            if (lst.empty() || lst.begin()->second != 1) {
+                lst.emplace_front(unordered_set<string>({key}), 1);
+            } else {
+                lst.begin()->first.insert(key);
+            }
+            dict[key] = lst.begin();
+        }
     }
-    auto node = dict[key];
-    node->val++;
-    while (node->next != tail && node->val > node->next->val) moveBack(node);
-  }
 
-  /** Decrements an existing key by 1. If Key's value is 1, remove it from the
-   * data structure. */
-  void dec(string key) {
-    if (!dict.count(key)) return;
-    auto node = dict[key];
-    node->val--;
-    while (node->prev != head && node->val < node->prev->val) moveFront(node);
-    if (!isEmpty() && head->next->val == 0) {
-      auto node = head->next;
-      head->next = head->next->next;
-      dict.erase(node->key);
-      //ATTENTION: memory leak!
-      // delete node;
+    void dec(string key) {
+        auto node = dict[key];
+        if (node->second == 1) {
+            dict.erase(key);
+        } else {
+            auto pre = prev(node);
+            if (node == lst.begin() || pre->second < node->second - 1) {
+                dict[key] = lst.emplace(node, unordered_set<string>({key}),
+                                        node->second - 1);
+            } else {
+                pre->first.insert(key);
+                dict[key] = pre;
+            }
+        }
+        node->first.erase(key);
+        if (node->first.empty()) lst.erase(node);
     }
-  }
 
-  /** Returns one of the keys with maximal value. */
-  string getMaxKey() { return tail->prev->key; }
+    string getMaxKey() {
+        return lst.empty() ? "" : *lst.rbegin()->first.begin();
+    }
 
-  /** Returns one of the keys with Minimal value. */
-  string getMinKey() { return head->next->key; }
+    string getMinKey() {
+        return lst.empty() ? "" : *lst.begin()->first.begin();
+    }
 };
-
 /**
  * Your AllOne object will be instantiated and called as such:
  * AllOne* obj = new AllOne();
@@ -110,31 +78,31 @@ class AllOne {
 // @lc code=end
 
 int main() {
-  ifstream in("input");
-  while (!in.eof()) {
-    string op, para;
-    in >> op >> para;
-    auto ops = getStringArray(op);
-    auto temp = getStringMatrix(para);
-    vector<string> paras;
-    for (auto i : temp) {
-      if (i.size())
-        paras.push_back(i[0]);
-      else
-        paras.push_back("");
-    }
+    ifstream in("input");
+    while (!in.eof()) {
+        string op, para;
+        in >> op >> para >> ws;
+        auto ops = getStringArray(op);
+        auto temp = getStringMatrix(para);
+        vector<string> paras;
+        for (auto i : temp) {
+            if (i.size())
+                paras.push_back(i[0]);
+            else
+                paras.push_back("");
+        }
 
-    auto a = AllOne();
-    for (int i = 1; i < ops.size(); i++) {
-      if (ops[i] == "inc")
-        a.inc(paras[i]);
-      else if (ops[i] == "getMaxKey")
-        cout << a.getMaxKey() << " ";
-      else if (ops[i] == "dec")
-        a.dec(paras[i]);
-      else if (ops[i] == "getMinKey")
-        cout << a.getMinKey() << " ";
+        auto a = AllOne();
+        for (int i = 1; i < ops.size(); i++) {
+            if (ops[i] == "inc")
+                a.inc(paras[i]);
+            else if (ops[i] == "getMaxKey")
+                cout << a.getMaxKey() << " ";
+            else if (ops[i] == "dec")
+                a.dec(paras[i]);
+            else if (ops[i] == "getMinKey")
+                cout << a.getMinKey() << " ";
+        }
+        cout << endl;
     }
-    cout<<endl;
-  }
 }
