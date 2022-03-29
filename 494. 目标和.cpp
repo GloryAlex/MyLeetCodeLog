9a35c9b238f3ebf9650c9f6cea483a43b3d6c92a
gloryxie <gloryxie98@outlook.com>
2022-03-29 14:57:48 +0800

    ✅ 494

--- 494. 目标和.cpp
+++ 494. 目标和.cpp
@@ -1,37 +1,37 @@
-#include "LeetCode.h"
+#include "lib/leetcode.h"
 using namespace std;
 
-class Solution
-{
-public:
-    int findTargetSumWays(vector<int> &nums, int s)
-    {
-        int length = nums.size();
-        auto dp = vector<vector<int>>(length,vector<int>(2001));
-
-        dp[0][nums[0]+1000]=1;
-        dp[0][-nums[0]+1000]=1;
-
-        for(int i=1;i<length;i++){
-            for(int j=-1000;j<=1000;j++){
-                if(dp[i-1][j+1000]>0){
-                    dp[i][j+1000-nums[i]]+=dp[i-1][j+1000];
-                    dp[i][j+1000+nums[i]]+=dp[i-1][j+1000];
-                }
-            }
+class Solution {
+    struct leetCodePairHash {
+       public:
+        template <typename T, typename U>
+        size_t operator()(const pair<T, U> &x) const {
+            return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
         }
-        return s>1000?0:dp.back().at(s+1000);
+    };
+    unordered_map<pair<int, int>, int, leetCodePairHash> dict;
+    int dfs(vector<int> &nums, int target, int i, int sum) {
+        if (dict.count({i, sum})) return dict[{i, sum}];
+        if (i == nums.size()) {
+            return dict[{i, sum}] = (sum == target);
+        }
+        return dict[{i, sum}] = dfs(nums, target, i + 1, sum + nums[i]) +
+                                dfs(nums, target, i + 1, sum - nums[i]);
+    }
+
+   public:
+    int findTargetSumWays(vector<int> &nums, int s) {
+        dict = unordered_map<pair<int, int>, int, leetCodePairHash>();
+        return dfs(nums, 0, 0, s);
     }
 };
 
-int main()
-{
+int main() {
     ifstream in("input");
-    while (!in.eof())
-    {
+    while (!in.eof()) {
         string str;
         int s;
-        in >> str >> s;
+        in >> str >> s >> ws;
         auto arr = getArray(str);
         print(Solution().findTargetSumWays(arr, s));
     }
