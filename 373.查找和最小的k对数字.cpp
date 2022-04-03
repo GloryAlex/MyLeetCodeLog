be9ef05b276a913ee9c3313f3b8055b4b6073806
gloryxie <gloryxie98@outlook.com>
2022-04-03 23:09:30 +0800

    ✅ 剑指 061

--- 373.查找和最小的k对数字.cpp
+++ 373.查找和最小的k对数字.cpp
@@ -1,50 +1,57 @@
-#include "LeetCode.h"
+#include "lib/leetcode.h"
 using namespace std;
 
-class Solution
-{
-public:
-    struct tuple
-    {
-        int a, b, c;
-
-        bool operator<(const tuple& B)const
-        {
-            return a + b > B.a + B.b;
+class Solution {
+    struct leetCodePairHash {
+       public:
+        template <typename T, typename U>
+        size_t operator()(const pair<T, U>& x) const {
+            return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
         }
     };
-    vector<vector<int>> kSmallestPairs(vector<int> &nums1, vector<int> &nums2, int k)
-    {
-        int len1 = nums1.size(), len2 = nums2.size();
-        if (!len1 || !len2)
-            return {};
-        if (len1 > len2)
-            return kSmallestPairs(nums2, nums1, k);
 
-        priority_queue<tuple, vector<tuple>> q;
-        for (int i : nums1)
-            q.push({i, nums2[0], 0});
+   public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
+                                       int k) {
+        if (nums1.empty() || nums2.empty()) return {};
+        int m = nums1.size(), n = nums2.size();
+        unordered_set<pair<int, int>, leetCodePairHash> s;
 
-        vector<vector<int>> ret;
-
-        while (ret.size() < k && !q.empty())
-        {
-            auto cur = q.top();
+        auto cmp = [&](const pair<int, int>& a, const pair<int, int>& b) {
+            return nums1[a.first] + nums2[a.second] >
+                   nums1[b.first] + nums2[b.second];
+        };
+        auto q = priority_queue<pair<int, int>, vector<pair<int, int>>,
+                                decltype(cmp)>(cmp);
+        auto isValid = [&](pair<int, int>& p) {
+            return p.first < m && p.second < n;
+        };
+        q.push({0, 0});
+        vector<vector<int>> res;
+        while (res.size() < k && !q.empty()) {
+            auto p = q.top();
             q.pop();
 
-            ret.push_back({cur.a, cur.b});
-            if (++cur.c < nums2.size())
-                q.push({cur.a, nums2[cur.c], cur.c});
+            res.push_back({nums1[p.first], nums2[p.second]});
+
+            auto next1 = pair<int, int>{p.first + 1, p.second};
+            auto next2 = pair<int, int>{p.first, p.second + 1};
+            if (!s.count(next1) && isValid(next1)) {
+                q.push(next1);
+                s.insert(next1);
+            }
+            if (!s.count(next2) && isValid(next2)) {
+                q.push(next2);
+                s.insert(next2);
+            }
         }
-        return ret;
+        return res;
     }
 };
 
-int main()
-{
+int main() {
     ifstream in("input");
-    while (!in.eof())
-    {
+    while (!in.eof()) {
         string str1, str2;
         int k;
         in >> str1 >> str2 >> k;
