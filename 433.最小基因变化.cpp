6845e331ca88cd1fb21e3094624fd65a7d93c95c
gloryxie <gloryxie98@outlook.com>
2022-05-07 23:07:00 +0800

    ✅ 433

--- 433.最小基因变化.cpp
+++ 433.最小基因变化.cpp
@@ -1,72 +1,71 @@
-#include "LeetCode.h"
+#include "lib/leetcode.h"
 using namespace std;
 
-class Solution
-{
-private:
-    const int NO_ANS = -1;
-    inline bool oneWordDiff(const string &a, const string &b)
-    {
-        int diff = 0;
-        for (int i = 0; i < a.size() && diff <= 1; i++)
-        {
-            if (a[i] != b[i])
-                diff++;
+/*
+ * @lc app=leetcode.cn id=433 lang=cpp
+ *
+ * [433] 最小基因变化
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        const int INVALID_MUTATE = -1;
+        auto validMove = [](const string& a, const string& b) {
+            int cnt = 0;
+            for (int i = 0; i < 8; i++) cnt += (a[i] != b[i]);
+            return cnt == 1;
+        };
+        // 添加初始
+        auto startPos = -1, endPos = -1;
+        for (int i = 0; i < bank.size(); i++) {
+            if (bank[i] == start)
+                startPos = i;
+            else if (bank[i] == end)
+                endPos = i;
         }
-        return diff == 1;
-    }
-
-public:
-    int minMutation(string start, string end, vector<string> &bank)
-    {
-        if(start == end)return 0;
-
-        int length=bank.size();
-        unordered_map<string,int> id;
-        queue<int> q;
-
-        for(int i=0;i<length;i++){
-            id[bank[i]]=i;
-            if(oneWordDiff(bank[i],start))q.push(i);
+        if (startPos == -1) {
+            startPos = bank.size();
+            bank.emplace_back(start);
         }
-
-        vector<int> hasVisited(bank.size());
-
-        int endId;
-        if(!id.count(end))return NO_ANS;
-        else endId=id[end];
-
-        int stepNums = 1;
-        while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                if(q.front()==endId)return stepNums;
-
-                int curId = q.front();
-                q.pop();
-
-                if(hasVisited[curId])continue;
-                else hasVisited[curId]=true;
-
-                for(int i=0;i<length;i++){
-                    if(!hasVisited[i] && oneWordDiff(bank[curId],bank[i]))
-                        q.push(i);
+        if (endPos == -1) return INVALID_MUTATE;
+        // 构建图
+        vector<vector<int>> grids(bank.size());
+        for (int i = 0; i < bank.size(); i++) {
+            for (int j = i + 1; j < bank.size(); j++) {
+                if (validMove(bank[i], bank[j])) {
+                    grids[i].push_back(j);
+                    grids[j].push_back(i);
                 }
             }
-            stepNums++;
         }
-        return NO_ANS;
+        // bfs
+        auto hasVisit = vector<int>(bank.size(), false);
+        auto q = queue<int>({startPos});
+        for (int steps = 0; !q.empty(); steps++) {
+            int size = q.size();
+            while (size--) {
+                auto cur = q.front();
+                q.pop();
+                if (cur == endPos) return steps;
+                hasVisit[cur] = true;
+                for (int next : grids[cur]) {
+                    if (hasVisit[next] == false) q.push(next);
+                }
+            }
+        }
+        return INVALID_MUTATE;
     }
 };
+// @lc code=end
 
-int main()
-{
+int main() {
     ifstream in("input");
-    while (!in.eof())
-    {
+    while (!in.eof()) {
         string m, n;
         string str;
-        in >> m >> n >> str;
+        in >> m >> n >> str >> ws;
         m = m.substr(1, m.size() - 2);
         n = n.substr(1, n.size() - 2);
         auto arr = getStringArray(str);
